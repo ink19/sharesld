2e894f9b95b23c0ebc9cac49bf568831feab12d1
myzly <nkzxwgr@163.com>
2016-07-22 15:11:41 +0800

    init

--- tp5/application/common.php
+++
@@ -1,4 +0,0 @@
-<?php
-use \think\Route;
-
-Route::rule('/love', 'index','GET');
--- tp5/application/index/model/User.php
+++
@@ -1,8 +0,0 @@
-<?
-namespace app\index\model;
-
-use think\Model;
-
-class User extends Model{
-    public function 
-}
